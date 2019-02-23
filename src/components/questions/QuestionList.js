727451c9f23fe4aa8102eb2f577c1c7e049e3e2b
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-02-23 13:36:51 +1100

    Improve on Question List layout

--- src/components/questions/QuestionList.js
+++ src/components/questions/QuestionList.js
@@ -2,7 +2,29 @@ import React from 'react';
 
 const QuestionList = () => {
     return (
-        <div>QUESTION LIST</div>
+        <div className="ui relaxed divided list">
+            <div className="item">
+                <i className="large list alternate outline middle aligned icon"></i>
+                <div className="content">
+                <p className="header">Semantic-Org/Semantic-UI</p>
+                <div className="description">Updated 10 mins ago</div>
+                </div>
+            </div>
+            <div className="item">
+                <i className="large list alternate outline middle aligned icon"></i>
+                <div className="content">
+                <p className="header">Semantic-Org/Semantic-UI-Docs</p>
+                <div className="description">Updated 22 mins ago</div>
+                </div>
+            </div>
+            <div className="item">
+                <i className="large list alternate outline middle aligned icon"></i>
+                <div className="content">
+                <p className="header">Semantic-Org/Semantic-UI-Meteor</p>
+                <div className="description">Updated 34 mins ago</div>
+                </div>
+            </div>
+        </div>
     )
 }
 
